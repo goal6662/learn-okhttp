0b2bad9483c337768c4e7138aacff01343f2bc28
Goal123 <1691836542@qq.com>
2024-02-28 10:30:50 +0800

    post demo

---
+++ ok-01-basic/src/main/java/com/goal/method/server/domain/Person.java
@@ -0,0 +1,13 @@
+package com.goal.method.server.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Person {
+
+    private String name;
+
+    private Integer age;
+}
